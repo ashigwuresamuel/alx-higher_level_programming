b120a53b2058b0fa3114e910c63e5dfed471bda2
ashigwuresamuel <emmmanuelsamuelashigwure@gmail.com>
2023-11-27 18:25:40 -0800

    Python Hello World

--- 10-check_cycle.c
+++ 10-check_cycle.c
@@ -10,7 +10,7 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list;
 	if (!list)
-	return (0);
+		return (0);
 
 	while (slow && fast && fast->next)
 	{
@@ -20,5 +20,5 @@ int check_cycle(listint_t *list)
 		return (1);
 	}
 
-return (0);
+	return (0);
 }
