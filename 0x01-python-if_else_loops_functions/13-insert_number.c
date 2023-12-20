f5319bb879c3ca703900bc88b8e84b80346f2916
ashigwuresamuel <emmmanuelsamuelashigwure@gmail.com>
2023-12-19 18:36:39 -0800

    function in C that inserts a number into a sorted singly linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,50 +1,34 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_node - Inserts a number into a sorted singly linked list.
- * @head: Double pointer to a singly linked list
- * @number: Value of the new node of the list.
- * Return: The address of the new node, or NULL if it failed now.
+ * insert_node - adds a number into a sorted singly-linked list.
+ * @head: Pointer to head of the list.
+ * @number: The number to add
+ * Return: If the function fails - NULL or 0.
  */
 
 listint_t *insert_node(listint_t **head, int number)
 {
-int flag = 0;
-	listint_t *new_node = NULL, *actual = NULL, *after = NULL;
+	listint_t *insert = *head;
+	listint_t *update;
 
-	if (head == NULL)
+	update = malloc(sizeof(listint_t));
+
+	if (!update)
 		return (NULL);
-			new_node = malloc(sizeof(listint_t));
-	if (!new_node)
-		return (NULL);
-			new_node->n = number, new_node->next = NULL;
-	if (*head == NULL)
-		{
-			*head = new_node;
-				return (*head);
-				}
-		actual = *head;
-			if (number <= actual->n)
-			{
-			new_node->next = actual, *head = new_node;
-				return (*head);
-			}
-	if (number > actual->n && !actual->next)
-			{
-	actual->next = new_node;
-		return (new_node);
-				}
-	after = actual->next;
-		while (actual)
-			{
-	if (!after)
-		actual->next = new_node, flag = 1;
-	else if (after->n == number)
-		actual->next = new_node, new_node->next = after, flag = 1;
-	else if (after->n > number && actual->n < number)
-																					actual->next = new_node, new_node->next = after, flag = 1;
-																				if (flag)
-																				break;																				after = after->next, actual = actual->next;																			}
-	return (new_node);
+	update->n = number;
+
+	if (insert == NULL || insert->n >= number)
+	{
+		update->next = insert;
+		*head = update;
+		return (update);
+	}
+
+	while (insert && insert->next && insert->next->n < number)
+		insert = insert->next;
+	update->next = insert->next;
+	insert->next = update;
+
+	return (update);
 }
