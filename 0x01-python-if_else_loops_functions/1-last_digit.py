e21ec0df060d3f25dddf3e068b8255b7e81282f5
ashigwuresamuel <emmmanuelsamuelashigwure@gmail.com>
2023-12-19 18:51:15 -0800

    program that prints numbers from 0 to 99.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number >= 0:
-    last_digit = number % 10
+
+# step one convert the number to a string
+number_string = str(number)
+
+# step two  extract the last digit
+last_digit = number_string[-1]
+
+# step 3  first handle the case where the number is negative
+if number < 0:
+    last_digit = "-" + last_digit
+
+# Print the original number and the alst digit
+print(f"Last digit of {number} is {last_digit} and", end=" ")
+
+# Process conditions based on the last digit
+if int(last_digit) > 5:
+    print(f"is greater than 5")
+elif int(last_digit) == 0:
+    print(f"is 0")
 else:
-    last_digit = ((-number % 10) * -1)
-
-    message = f"Last digit of {number} is {last_digit}"
-
-if last_digit == 0:
-
-    print(f"{message} and is 0")
-elif last_digit > 5 and last_digit % 10 != 0:
-    print(f"{message} and is greater than 5")
-else:
-    print(f"{message} and is less than 6 and not 0")
+    print(f"is less than 6 and not 0")
