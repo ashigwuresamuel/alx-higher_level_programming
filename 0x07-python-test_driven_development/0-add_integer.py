302d9baf73b519d8ba5c2d1f28bd678e0c24feb5
ashigwuresamuel <emmmanuelsamuelashigwure@gmail.com>
2024-01-02 18:41:51 -0800

    python-test_driven_development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
-"""Module for add_integer method."""
+
+"""
+Function that adds two integers/float numbers.
+prototype: def add_integer(a, b=98):
+Returns an integer: the addition of a and b
+"""
 
 
 def add_integer(a, b=98):
-    """Adds two integers.
-
-    Args:
-        a: the first integer.
-        b: the second integer, default 98.
-
-    Raises:
-        TypeError: if a, b are not int, float.
-
-    Returns:
-        The sum of the two integers.
     """
+    Addition function of two integers.
+    Args:
+        a (int or float): The first number to be added.
+        b (int or float, optional): Second number to be added. Defaults to 98.
+    Raises:
+        TypeError: If either `a` or `b` is not an integer or float.
+    Returns:
+        int: The addition of `a` and `b` as an integer.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
 
-    if type(a) not in (int, float):
-        raise TypeError('a must be an integer')
-    if type(b) not in (int, float):
-        raise TypeError('b must be an integer')
-    return int(a) + int(b)
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    # cast numbers into integers if they are float
+    a = int(a)
+    b = int(b)
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/0-add_integer.txt")
+    return int(a) + int(b))
