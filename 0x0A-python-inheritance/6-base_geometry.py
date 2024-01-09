07651f64e98f8f934964da00dead1b3e9caf5b3f
ashigwuresamuel <emmmanuelsamuelashigwure@gmail.com>
2024-01-08 18:53:49 -0800

    Base Geomaetry with public instance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Module 6-base_geometry
+Contains empty class BaseGeometry
+with public instance method area
+"""
+
+
+class BaseGeometry:
+    """
+    A Base geometry class
+    Method:
+        area(self)
+    """
+
+    def area(self):
+        """
+        area function that is empty
+        (Unimplemented)
+        """
+        raise Exception('area() is not implemented')
