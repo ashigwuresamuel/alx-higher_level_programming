41fa1dc6d2f6b36598b2f8d47104be878c4b015a
Ashigwure Samuel <emmanuesamuelashigwure@gmail..com>
2024-03-21 03:31:29 +0000

    0x0F. Python - Object-relational mapping|

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,52 +1,18 @@
 #!/usr/bin/python3
-
-"""
-Script that displays all values in the states table of hbtn_0e_0_usa
-where name matches the provided argument.
-Parameters for script: mysql username, mysql password, database name
-and state name searched.
-Must use the `MySQLdb` module.
-Script should connect to a MySQL server runnimg on `localhost` at port `3306`
-Must use `format` to create the SQL query with the user input.
-Results must be in ascending order by `states.id`.
-Code should not be executed when imported.
-"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    # add check to see if number of arguments is correct
-    if len(argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(
-            argv[0]))
-        exit()
-
-    # establishing a secure connection to the MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
-
-    # creating a cursor object to execute SQL queries
-    cursor = db.cursor()
-
-    # using format to create the SQL query with the user input
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-            argv[4])
-    cursor.execute(query)
-
-    # fetching all the results
-    states = cursor.fetchall()
-
-    # display/print them out
-    for state in states:
-        if state[1] == argv[4]:
-            print(state)
-
-    # closing the cursor and database connection
-    cursor.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
     db.close()
+
